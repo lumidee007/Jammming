9e40a5277d0ad56d8f2d79bcaca5811d1f97a53b
Oluwafemi Ayinla <oluwafemi.ayinla@gmail.com>
2023-03-30 22:46:31 +0100

    add SearchResults component and styling

---
+++ src/Components/SearchResults/SearchResults.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./SearchResults.styles.css";
+
+export default function SearchResults() {
+  return (
+    <div className="SearchResults">
+      <h2>Results</h2>
+    </div>
+  );
+}
